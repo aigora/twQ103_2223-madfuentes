8db1871e34afae7079f32093eaa41c2703913127
hfjfbfb <130643665+hfjfbfb@users.noreply.github.com>
2023-04-25 13:27:48 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//MENÚ
+int main (){
+	int option;
+	printf("BIENVENIDO AL REGISTRO DE CALIDAD Y PROPIEDADES DE FUENTES DE MADFUENTES:\n");
+	printf("¿Qué desea hacer?\n");
+	printf("-Consultar información sobre las fuentes (PRESIONE 1)\n");
+	printf("-Modificar información sobre las fuentes (PRESIONE 2)\n");
+	scanf("%c",&option);
+	switch (option){
+		case '1':
+			printf("A");//EL PROGRAMA IMPRIME LOS DATOS GUARDADOS
+	}
+	switch (option){
+		case '2':
+			printf("B");//EL PRONGRMA IMPREIME LOS DATOS GUARDADOS Y LE PIDE AL USUARIO QUE DESEA CAMBIAR
+	}
+	return 0;
+}
